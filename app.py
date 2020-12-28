9ebc35027bddbcb898128ea3cfb0bfcc92e8dd9a
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2020-12-28 16:42:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,34 +3,40 @@ import streamlit.components.v1 as components
 import networkx as nx
 import matplotlib.pyplot as plt
 from pyvis.network import Network
+import got 
 #Network(notebook=True)
-st.title('Hello Networkx')
+st.title('Hello Pyvis')
+# make Network show itself with repr_html
 
-def net_repr_html(self):
-  nodes, edges, height, width, options = self.get_network_data()
-  html = self.template.render(height=height, width=width, nodes=nodes, edges=edges, options=options)
-  return html
+#def net_repr_html(self):
+#  nodes, edges, height, width, options = self.get_network_data()
+#  html = self.template.render(height=height, width=width, nodes=nodes, edges=edges, options=options)
+#  return html
 
-Network._repr_html_ = net_repr_html
+#Network._repr_html_ = net_repr_html
+st.sidebar.title('Choose your favorite Graph')
+option=st.sidebar.selectbox('select graph',('Simple','Karate', 'GOT'))
+physics=st.sidebar.checkbox('add physics interactivity?')
+got.simple_func(physics)
 
-import networkx as nx
-nx_graph = nx.cycle_graph(10)
-nx_graph.nodes[1]['title'] = 'Number 1'
-nx_graph.nodes[1]['group'] = 1
-nx_graph.nodes[3]['title'] = 'I belong to a different group!'
-nx_graph.nodes[3]['group'] = 10
-nx_graph.add_node(20, size=20, title='couple', group=2)
-nx_graph.add_node(21, size=15, title='couple', group=2)
-nx_graph.add_edge(20, 21, weight=5)
-nx_graph.add_node(25, size=25, label='lonely', title='lonely node', group=3)
+if option=='Simple':
+  HtmlFile = open("test.html", 'r', encoding='utf-8')
+  source_code = HtmlFile.read() 
+  components.html(source_code, height = 900,width=900)
 
-from pyvis.network import Network
-nt = Network("800px", "800px",heading='Hello Pyvis')
-nt.from_nx(nx_graph)
-nt.show('test.html')
-#components.html(test.html)
 
-HtmlFile = open("test.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-#print(source_code)
-components.html(source_code, height = 900,width=900)
+got.got_func(physics)
+
+if option=='GOT':
+  HtmlFile = open("gameofthrones.html", 'r', encoding='utf-8')
+  source_code = HtmlFile.read() 
+  components.html(source_code, height = 1200,width=1000)
+
+
+
+got.karate_func(physics)
+
+if option=='Karate':
+  HtmlFile = open("karate.html", 'r', encoding='utf-8')
+  source_code = HtmlFile.read() 
+  components.html(source_code, height = 1200,width=1000)
